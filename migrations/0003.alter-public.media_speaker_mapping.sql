d2ddd8f8b623f15a6260dad3ac9a39eebd2d8118
unknown <vaibhavsinghal2000@gmail.com>
2024-06-08 13:24:00 +0530

    commented query in 0003.alter-public.media_speaker>mapping.sql file

--- migrations/0003.alter-public.media_speaker_mapping.sql
+++ migrations/0003.alter-public.media_speaker_mapping.sql
@@ -3,6 +3,7 @@ ALTER TABLE public.media_speaker_mapping
 ADD COLUMN LABELLED_ARTIFACT_NAME VARCHAR
 
 
--- depends: 0001.create-public.media_speaker_mapping
-ALTER TABLE public.media_speaker_mapping
-ADD COLUMN UNLABELLED_ARTIFACT_NAME VARCHAR
+-- -- depends: 0001.create-public.media_speaker_mapping
+-- ALTER TABLE public.media_speaker_mapping
+-- ADD COLUMN UNLABELLED_ARTIFACT_NAME VARCHAR
+-- commenting this to debug the error by vaib on 08-06-2024
