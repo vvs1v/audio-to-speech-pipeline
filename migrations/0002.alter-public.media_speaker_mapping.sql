8b48e2c5e140e886638efa486b6cfaa2cd61b4e9
unknown <vaibhavsinghal2000@gmail.com>
2024-06-08 12:57:45 +0530

    commented drop query in 0002.alter-public.media_speaker>mapping.sql file

--- migrations/0002.alter-public.media_speaker_mapping.sql
+++ migrations/0002.alter-public.media_speaker_mapping.sql
@@ -2,6 +2,8 @@
 ALTER TABLE public.media_speaker_mapping
 ADD COLUMN ARTIFACT_NAME VARCHAR
 
--- depends: 0001.create-public.media_speaker_mapping
-ALTER TABLE public.media_speaker_mapping
-DROP COLUMN IF EXISTS ARTIFACT_NAME
+-- -- depends: 0001.create-public.media_speaker_mapping
+-- ALTER TABLE public.media_speaker_mapping
+-- DROP COLUMN IF EXISTS ARTIFACT_NAME
+-- commenting this to debug the error by vaib on 08-06-2024
+
